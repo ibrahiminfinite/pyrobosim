2ff760fe72234d1f6d43967134be3063dfb3de09
ibrahiminfinite <ibrahimjkd@@gmail.com>
2023-03-11 22:24:59 +0530

    Add free pos sampling

--- pyrobosim/pyrobosim/navigation/prm_grid.py
+++ pyrobosim/pyrobosim/navigation/prm_grid.py
@@ -1,5 +1,9 @@
 """ Grid based PRM (Probablistic Roadmap) implementation. """
 
+import time
+import numpy as np
+from pyrobosim.navigation.occupancy_grid import occupancy_grid_from_world
+
 
 class PRMGridPlanner:
     """
@@ -11,10 +15,11 @@ class PRMGridPlanner:
         world,
         resolution=0.05,
         inflation_radius=0.15,
-        max_nodes=100,
+        max_node_samples=1000,
+        num_nodes=100,
         max_connection_dist=2.0,
         compress_path=True,
-        max_time=5.0,
+        max_planning_time=5.0,
     ) -> None:
         """
         Creates a grid based PRM Planner
@@ -25,17 +30,42 @@ class PRMGridPlanner:
         :type resolution: float
         :param inflation_radius: The inflation radius to be used in the occupancy grid, in meters.
         :type inflation_radius: float
-        :param max_nodes: Maximum nodes to be sampled to build the PRM.
-        :type max_nodes: int
+        :param max_node_samples: Maximum nodes to be sampled to build the PRM.
+        :type max_node_samples: int
+        :param num_nodes: The desired number of nodes in the PRM graph (ideally less then `max_node_samples`).
+        :type num_nodes: int
         :param max_connection_dist: Maximum connection distance between nodes.
         :type max_connection_dist: float
         :param compress_path: If true, applies waypoint reduction to generated path, else returns full path.
         :type compress_path: bool
-        :param max_time: The maximum time allowed for planning.
-        :type max_time: float
+        :param max_planning_time: The maximum time allowed for planning.
+        :type max_planning_time: float
         """
 
-        pass
+        self.world = world
+        self.resolution = resolution
+        self.inflation_radius = inflation_radius
+        self.max_node_samples = max_node_samples
+        self.num_nodes = num_nodes
+        self.max_connection_dist = max_connection_dist
+        self.compress_path = compress_path
+        self.max_planning_time = max_planning_time
+
+        # Data structures for planning
+        self.free_pos = np.zeros((self.num_nodes, 2), dtype=np.uint16)
+        self._set_occupancy_grid()
+
+    def _set_occupancy_grid(self):
+        """
+        Generates occupancy grid of specified configuration
+        """
+        ts = time.time()
+        self.grid = occupancy_grid_from_world(
+            self.world,
+            resolution=self.resolution,
+            inflation_radius=self.inflation_radius,
+        )
+        self.grid_generation_time = time.time() - ts
 
     def _distance(self, nodeA, nodeB):
         """
@@ -54,7 +84,7 @@ class PRMGridPlanner:
     def _distances_all(self, node):
         """
         Computes the distance from given node to all other nodes in the graph.
-        
+
         :param node: The node from which to find distances to all other nodes.
         :type node: [int, int]
         :retrun: Distance from node to all other nodes.
@@ -75,6 +105,25 @@ class PRMGridPlanner:
         """
 
         pass
+
+    def _sample_free_nodes(self):
+        """
+        Samples `num_nodes` in the free space.
+
+        :return: True if successfully samples required number of free positions, else False
+        :rtype: bool
+        """
+        # Note: uint16 can only handle grids up to shape of (65535, 65535)
+        xs = np.random.randint(0, self.grid.width, self.max_node_samples, np.dtype)
+        ys = np.random.randint(0, self.grid.height, self.max_node_samples, np.dtype)
+        i = 0
+        j = 0
+        while i < self.max_node_samples and j < self.num_nodes:
+            x, y = xs[i], ys[i]
+            if not self.grid.is_occupied((x, y)):
+                self.free_pos[j, :] = [x, y]
+                j += 1
+            i += 1
 
     def plan(self, start, goal):
         """
